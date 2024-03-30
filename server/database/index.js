9df39c9ffd7abdc81ef6ecc137946f6b9eca4abe
crimsonriffic <50260908+crimsonriffic@users.noreply.github.com>
2024-03-29 20:30:29 -0400

    merged

--- server/database/index.js
+++ server/database/index.js
@@ -1,11 +1,11 @@
 const  Pool  = require('pg').Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "",
-    host: "localhost",
-    port: 5432,
-    database: "postgres"
+    // user: "postgres",
+    // password: "",
+    // host: "localhost",
+    // port: 5432,
+    // database: "postgres"
 })
 
 module.exports = {
